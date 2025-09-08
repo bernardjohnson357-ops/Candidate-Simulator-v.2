0fd084f5bf447e20791952290d41dd98ecd5233f
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-09-08 16:29:56 -0500

    Refine SpeechRecognition types in global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,12 @@
 // global.d.ts
+
+// These tell TypeScript about the SpeechRecognition constructor on window
 interface Window {
-  SpeechRecognition: any;
-  webkitSpeechRecognition: any;
+  SpeechRecognition: typeof window.SpeechRecognition | undefined;
+  webkitSpeechRecognition: typeof window.webkitSpeechRecognition | undefined;
 }
 
+// Use `any` for the instance type
 type SpeechRecognition = any;
 
 export {};

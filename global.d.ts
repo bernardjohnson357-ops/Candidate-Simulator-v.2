c62bf630716bd9af6a82e863ff62b1647d2fc8b1
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-09-08 17:44:36 -0500

    Refactor SpeechRecognition declarations in global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,15 @@
 // global.d.ts
 
-// Make SpeechRecognition available globally to TypeScript
-declare var SpeechRecognition: any;
-declare var webkitSpeechRecognition: any;
+// Declare both the type and the constructor globally
+type SpeechRecognition = any;
+type SpeechRecognitionEvent = any;
 
 interface Window {
   SpeechRecognition: typeof SpeechRecognition;
-  webkitSpeechRecognition: typeof webkitSpeechRecognition;
+  webkitSpeechRecognition: typeof SpeechRecognition;
 }
+
+declare var SpeechRecognition: any;
+declare var webkitSpeechRecognition: any;
 
 export {};

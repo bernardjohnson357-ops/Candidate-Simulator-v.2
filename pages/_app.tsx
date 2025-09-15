b216522e3fed2793c7ceb3e51acf6d915e110efe
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-09-14 22:42:54 -0500

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+// pages/_app.tsx
+import { GameStateProvider } from "@/context/GameStateContext";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <GameStateProvider>
+      <Component {...pageProps} />
+    </GameStateProvider>
+  );
+}

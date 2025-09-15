6e51e3563ecc5289fc79333c6899425a6f4a7c9a
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-09-15 10:49:07 -0500

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -18,3 +18,32 @@ export interface QuizResult {
   signaturesEarned: number;
   ccBonus: number;
 }
+
+// Add this GameState export:
+export interface GameState {
+  cc: number;
+  signatures: number;
+  voterApproval: number;
+  currentModule:
+    | "0"
+    | "1A"
+    | "1B"
+    | "2A"
+    | "2B"
+    | "3"
+    | "4"
+    | "5"
+    | "6"
+    | "7"
+    | "8"
+    | "9"
+    | "10"
+    | "11"
+    | "12"
+    | "13"
+    | "14"
+    | "15"
+    | "GeneralElection";
+  branch: "1A" | "1B" | "2A" | "2B" | null;
+  quizzesCompleted?: string[];
+}

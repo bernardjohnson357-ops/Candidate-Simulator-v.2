94840ba1de976356a73cc6dabca535e0e90393a4
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-09-22 15:05:53 -0500

    Update aiLoop.ts

--- app/ai/aiLoop.ts
+++ app/ai/aiLoop.ts
@@ -1,5 +1,5 @@
 // ./app/ai/aiLoop.ts
-import { libertarianSimulator } from "./utils/libertarianSimulator";
+import { libertarianSimulator } from "./app/utils/libertarianSimulator";
 import { CandidateState, ModuleState, Module, Task } from "./types";
 
 // ------------------------------

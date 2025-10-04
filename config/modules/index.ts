cff2d450ef620dad876c924f5753d0b777fcd898
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-10-04 06:47:06 -0500

    Update index.ts

--- config/modules/index.ts
+++ config/modules/index.ts
@@ -1,4 +1,6 @@
 import { module0 } from "./module0";
 import { module1 } from "./module1";
+import { module2 } from "./module2";
+import { Module } from "@/app/ai/types";
 
-export const modules = [module0, module1];
+export const modules: Module[] = [module0, module1, module2];

b8fc4615daf3d1d6b2caf2a89cf71c3ab0a95488
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-10-04 18:16:54 -0500

    Update aiLoop.ts

--- app/ai/aiLoop.ts
+++ app/ai/aiLoop.ts
@@ -1,27 +1,27 @@
 // ./app/ai/aiLoop.ts
+
 import { CandidateState } from "./types";
 
 export const initCandidateState = (office: "President" | "Senate" | "House"): CandidateState => {
-  let cc = 50; // default starting Candidate Coins
-  let threshold = { cc: 0, approval: 0, sigs: undefined };
+  let threshold = { cc: 0, approval: 0, sigs: 0 };
 
   switch (office) {
     case "President":
-      threshold = { cc: 75, approval: 2.5 };
+      threshold = { cc: 75, approval: 2.5, sigs: 0 };
       break;
     case "Senate":
-      threshold = { cc: 50, approval: 2.5 };
+      threshold = { cc: 50, approval: 2.5, sigs: 0 };
       break;
     case "House":
-      threshold = { cc: 31, approval: 2.5 };
+      threshold = { cc: 31, approval: 2.5, sigs: 0 };
       break;
   }
 
   return {
     office,
-    cc,
-    signatures: 0,
-    approval: 0,
+    cc: 50,           // Starting Candidate Coins
+    signatures: 0,    // Starting signatures
+    approval: 0,      // Starting voter approval %
     threshold,
   };
 };

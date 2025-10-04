de200825a481bd882104c3270add6176cc6ccf51
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-10-04 16:20:22 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 "use client";
 
 import React, { useState } from "react";
-import ModuleDisplay from "../components/ModuleDisplay";
-import module0Data from "../data/modules/module0.json";
+import ModuleDisplay from "@/components/ModuleDisplay";
+import module0Data from "@/data/modules/module0.json";
 
 export default function HomePage() {
   const [module] = useState(module0Data);

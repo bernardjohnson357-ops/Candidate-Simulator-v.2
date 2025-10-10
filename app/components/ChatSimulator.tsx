591af59066e952f76a16201413bf257e474ac52b
bernardjohnson357-ops <bernardjohnson357@gmail.com>
2025-10-10 15:02:19 -0500

    Update ChatSimulator.tsx

--- app/components/ChatSimulator.tsx
+++ app/components/ChatSimulator.tsx
@@ -3,8 +3,8 @@
 
 import React, { useEffect, useState } from "react";
 import { speak } from "../utils/audioUtils";
-import { allModules } from "@/app/data/allModules";
-import { Module, Task } from "@/app/ai/types";
+import { allModules } from "../app/data/allModules";
+import { Module, Task } from "../app/ai/types";
 
 // ---------- TYPES ----------
 interface CandidateState {
